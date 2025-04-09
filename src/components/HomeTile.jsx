ee6de3d2e67b1687b6663f28900ddb3c19149439
Nalla06 <lakshmi.rajyam06@gmail.com>
2025-04-09 12:38:48 +0200

    Files Updated

--- src/components/HomeTile.jsx
+++ src/components/HomeTile.jsx
@@ -18,7 +18,6 @@ function ListItem(props) {
   //       });
   //     },
   //     { rootMargin: "100% 0% 100% 0%" }
-  //     // { threshold: 1 }
   //   );
   //   observer.observe(event);
   // }
@@ -31,6 +30,20 @@ function ListItem(props) {
   // setTimeout(() => {
   //   console.log("Delayed for 1 second.");
   // }, 1000);
+
+  // Example event handlers for the buttons
+  const handlePlay = () => {
+    console.log('Play clicked');
+    // Add functionality to play
+  };
+  const handleAddToWatchlist = () => {
+    console.log('Added to watchlist');
+    // Add functionality for adding to watchlist
+  };
+  const handleInfo = () => {
+    console.log('Info clicked');
+    // Add functionality to show details
+  };
 
   return (
     <li
@@ -67,13 +80,12 @@ function ListItem(props) {
           </div>
           <section className="home-tile-details-section">
             <h2 className="home-tile-details-poster">
-              <a href="">
+              <a href="https://www.example.com">
                 <div className="home-tile-details-img-div">
                   <img
                     className="home-tile-details-img"
                     src={props.images}
-                    // src={props.logo}
-                    alt="tite art"
+                    alt="title art"
                   ></img>
                 </div>
               </a>
@@ -81,7 +93,6 @@ function ListItem(props) {
             <div className="home-tile-details-notposter-div1">
               <div className="home-tile-details-notposter-div2">
                 <span className="home-tile-details-notposter-span1">
-                  {/* basic information */}
                   {props.released}
                 </span>
                 <span className="home-tile-details-notposter-span2">
@@ -92,7 +103,10 @@ function ListItem(props) {
                 <div className="home-tile-details-buttons-play-div1">
                   <div className="home-tile-details-buttons-play-div2 ">
                     <label className="home-tile-details-buttons-play-label">
-                      <a className="home-tile-details-buttons-play-a" href="">
+                      <button
+                        className="home-tile-details-buttons-play-a"
+                        onClick={handlePlay}
+                      >
                         <svg
                           className="_22qEau"
                           viewBox="0 0 80 80"
@@ -102,27 +116,22 @@ function ListItem(props) {
                           style={{ height: "100%", width: "100%" }}
                         >
                           <title>Play Button</title>
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 80 80"
-                          >
-                            <path
-                              fill="currentColor"
-                              fillRule="evenodd"
-                              d="M40 80c22.091 0 40-17.909 40-40S62.091 0 40 0 0 17.909 0 40s17.909 40 40 40ZM29.761 55.536V25.575c0-1.724 1.877-2.791 3.359-1.91l25.178 14.98c1.447.862 1.447 2.959 0 3.82L33.12 57.446c-1.482.882-3.359-.186-3.359-1.91Z"
-                              clipRule="evenodd"
-                            ></path>
-                          </svg>
-                          <title>Play Button</title>
+                          <path
+                            fill="currentColor"
+                            fillRule="evenodd"
+                            d="M40 80c22.091 0 40-17.909 40-40S62.091 0 40 0 0 17.909 0 40s17.909 40 40 40ZM29.761 55.536V25.575c0-1.724 1.877-2.791 3.359-1.91l25.178 14.98c1.447.862 1.447 2.959 0 3.82L33.12 57.446c-1.482.882-3.359-.186-3.359-1.91Z"
+                          ></path>
                         </svg>
-                      </a>
-                      <span>Play</span>
+                        <span>Play</span>
+                      </button>
                     </label>
                   </div>
                 </div>
                 <div className="home-tile-details-buttons-watchlist-div">
-                  <button className="home-tile-details-buttons-watchlist-button">
+                  <button
+                    className="home-tile-details-buttons-watchlist-button"
+                    onClick={handleAddToWatchlist}
+                  >
                     <svg
                       viewBox="0 0 24 24"
                       height="24px"
@@ -132,28 +141,24 @@ function ListItem(props) {
                       style={{ height: 24, width: 24 }}
                     >
                       <title>Add</title>
-                      <svg
-                        width="24px"
-                        height="24px"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <title>Add</title>
-                        <path
-                          d="M3 12h18m-9 9V3"
-                          stroke="currentColor"
-                          strokeWidth="2"
-                          fill="none"
-                          strokeLinecap="round"
-                        ></path>
-                      </svg>
+                      <path
+                        d="M3 12h18m-9 9V3"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        fill="none"
+                        strokeLinecap="round"
+                      ></path>
                     </svg>
                   </button>
                   <label className="home-tile-details-buttons-labels">
-                    Watchlist ***xDisplayed on Hover***
+                    Watchlist
                   </label>
                 </div>
                 <div className="home-tile-details-buttons-info-div">
-                  <a href="" className="home-tile-details-buttons-info-a">
+                  <button
+                    className="home-tile-details-buttons-info-a"
+                    onClick={handleInfo}
+                  >
                     <svg
                       viewBox="0 0 24 24"
                       height="24"
@@ -163,28 +168,21 @@ function ListItem(props) {
                       style={{ height: 24, width: 24 }}
                     >
                       <title>Info</title>
-                      <svg
-                        width="24"
-                        height="24"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <title>Info</title>
-                        <g fill="none" fillRule="evenodd">
-                          <path
-                            d="M11 10.097c0-.603.439-1.097.976-1.097h.049c.536 0 .975.494.975 1.097v6.805c0 .604-.439 1.098-.975 1.098h-.05c-.536 0-.975-.494-.975-1.098v-6.805zM11 6h2v2h-2z"
-                            fill="currentColor"
-                          ></path>
-                          <circle
-                            stroke="currentColor"
-                            strokeWidth="2"
-                            cx="12"
-                            cy="12"
-                            r="9"
-                          ></circle>
-                        </g>
-                      </svg>
+                      <g fill="none" fillRule="evenodd">
+                        <path
+                          d="M11 10.097c0-.603.439-1.097.976-1.097h.049c.536 0 .975.494.975 1.097v6.805c0 .604-.439 1.098-.975 1.098h-.05c-.536 0-.975-.494-.975-1.098v-6.805zM11 6h2v2h-2z"
+                          fill="currentColor"
+                        ></path>
+                        <circle
+                          stroke="currentColor"
+                          strokeWidth="2"
+                          cx="12"
+                          cy="12"
+                          r="9"
+                        ></circle>
+                      </g>
                     </svg>
-                  </a>
+                  </button>
                   <label className="home-tile-details-buttons-labels">
                     Details
                   </label>
